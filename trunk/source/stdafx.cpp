b5440db5d57fd5a4235110947fce57dafd001aeb
sunjw8888 <sunjw8888@9d2edc16-0e2a-0908-9cea-4f64f3160d6d>
2011-05-02 12:10:06 +0000

    修正 VS08 编译变得很大的问题

--- trunk/source/stdafx.cpp
+++ trunk/source/stdafx.cpp
@@ -4,4 +4,30 @@
 
 #include "stdafx.h"
 
+// Fix so big binary
+// this is our own local copy of the AfxLoadSystemLibraryUsingFullPath function
+HMODULE AfxLoadSystemLibraryUsingFullPath(const WCHAR *pszLibrary)
+{
+	WCHAR wszLoadPath[MAX_PATH+1];
+	if (::GetSystemDirectoryW(wszLoadPath, _countof(wszLoadPath)) == 0)
+	{
+		return NULL;
+	}
+
+	if (wszLoadPath[wcslen(wszLoadPath)-1] != L'\\')
+	{
+		if (wcscat_s(wszLoadPath, _countof(wszLoadPath), L"\\") != 0)
+		{
+			return NULL;
+		}
+	}
+
+	if (wcscat_s(wszLoadPath, _countof(wszLoadPath), pszLibrary) != 0)
+	{
+		return NULL;
+	}
+
+	return(::AfxCtxLoadLibraryW(wszLoadPath));
+}
+
 
--- trunk/source/stdafx.h
+++ trunk/source/stdafx.h
@@ -38,4 +38,3 @@
 #ifndef _AFX_NO_AFXCMN_SUPPORT
 #include <afxcmn.h>			// Windows 公共控件的 MFC 支持
 #endif // _AFX_NO_AFXCMN_SUPPORT
-
