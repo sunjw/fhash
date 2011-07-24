447cf545366a47f9da9ef7d946122cad4d64c268
sunjw8888 <sunjw8888@9d2edc16-0e2a-0908-9cea-4f64f3160d6d>
2011-07-24 07:31:02 +0000

    stdafx.cpp 去掉实际内容

--- source/stdafx.cpp
+++ source/stdafx.cpp
@@ -1,33 +1,32 @@
 // stdafx.cpp : 只包括标准包含文件的源文件
-// MD5SUM03.pch 将是预编译头
 // stdafx.obj 将包含预编译类型信息
 
 #include "stdafx.h"
 
 // Fix so big binary
 // this is our own local copy of the AfxLoadSystemLibraryUsingFullPath function
-HMODULE AfxLoadSystemLibraryUsingFullPath(const WCHAR *pszLibrary)
-{
-	WCHAR wszLoadPath[MAX_PATH+1];
-	if (::GetSystemDirectoryW(wszLoadPath, _countof(wszLoadPath)) == 0)
-	{
-		return NULL;
-	}
-
-	if (wszLoadPath[wcslen(wszLoadPath)-1] != L'\\')
-	{
-		if (wcscat_s(wszLoadPath, _countof(wszLoadPath), L"\\") != 0)
-		{
-			return NULL;
-		}
-	}
-
-	if (wcscat_s(wszLoadPath, _countof(wszLoadPath), pszLibrary) != 0)
-	{
-		return NULL;
-	}
-
-	return(::AfxCtxLoadLibraryW(wszLoadPath));
-}
+//HMODULE AfxLoadSystemLibraryUsingFullPath(const WCHAR *pszLibrary)
+//{
+//	WCHAR wszLoadPath[MAX_PATH+1];
+//	if (::GetSystemDirectoryW(wszLoadPath, _countof(wszLoadPath)) == 0)
+//	{
+//		return NULL;
+//	}
+//
+//	if (wszLoadPath[wcslen(wszLoadPath)-1] != L'\\')
+//	{
+//		if (wcscat_s(wszLoadPath, _countof(wszLoadPath), L"\\") != 0)
+//		{
+//			return NULL;
+//		}
+//	}
+//
+//	if (wcscat_s(wszLoadPath, _countof(wszLoadPath), pszLibrary) != 0)
+//	{
+//		return NULL;
+//	}
+//
+//	return(::AfxCtxLoadLibraryW(wszLoadPath));
+//}
 
 
