e9a5d868da40e4a65132fb1e88e783312ad99375
sunjw8888@gmail.com <sunjw8888@gmail.com@9d2edc16-0e2a-0908-9cea-4f64f3160d6d>
2012-12-16 12:56:30 +0000

    发布 1.5.11

---
+++ tags/1.5.11/source/resource.h
@@ -0,0 +1,53 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by fileshash.rc
+//
+#define IDD_MAIN_DIALOG                 102
+#define IDD_ABOUT                       103
+#define IDI_ICON1                       129
+#define IDR_241                         133
+#define IDD_FIND_DIALOG                 134
+#define IDE_TXTMAIN                     1000
+#define IDC_STOP                        1002
+#define IDC_OPEN                        1003
+#define IDC_EXIT                        1004
+#define IDC_ABOUT                       1005
+#define IDC_PROGRESS                    1006
+#define IDC_MAIN                        1008
+#define IDC_ICON2                       1012
+#define IDC_PROG_WHOLE                  1014
+#define IDC_STATIC_FILE                 1015
+#define IDC_STATIC_WHOLE                1016
+#define IDC_STATIC_FILE2                1016
+#define IDC_STATIC_HASH                 1016
+#define IDC_CLEAN                       1017
+#define IDC_COPY                        1018
+#define IDC_CHECKUP                     1019
+#define IDC_STATIC_TIME                 1020
+#define IDC_STATIC_STR1                 1021
+#define IDC_STATIC_TIMETITLE            1021
+#define IDC_STATIC_SPEED                1022
+#define IDC_ADDReg                      1023
+#define IDC_STATIC_WHOLE2               1023
+#define IDC_STATIC_UPPER                1023
+#define IDC_STATIC_ADDRESULT            1024
+#define IDC_STATIC_ADDRESULT2           1025
+#define IDC_FIND                        1025
+#define IDC_EDIT_FILE                   1026
+#define IDC_EDIT2                       1027
+#define IDC_EDIT_HASH                   1027
+#define IDC_BUTTON1                     1028
+#define IDC_CONTEXT                     1028
+#define IDC_SYSLINK1                    1029
+#define IDC_SYSLINK_SITE                1029
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        135
+#define _APS_NEXT_COMMAND_VALUE         32772
+#define _APS_NEXT_CONTROL_VALUE         1030
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
