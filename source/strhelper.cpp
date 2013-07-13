46865c02883b770c8a148a3cbc3cbb4b4d60c596
sunjw8888@gmail.com <sunjw8888@gmail.com@9d2edc16-0e2a-0908-9cea-4f64f3160d6d>
2013-07-13 07:20:47 +0000

    合并新版 strhelper

--- source/strhelper.cpp
+++ source/strhelper.cpp
@@ -1,7 +1,7 @@
 /*
  * strhelper implementation file
  * Author: Sun Junwen
- * Version: 1.2.3
+ * Version: 1.3
  * Provides converting from tstring, string and wstring to each other
  * And provides string's utf8 converting.
  * Provides triming function to string and wstring.
@@ -12,12 +12,27 @@
 
 #include <string>
 
+#if defined (WIN32)
 #include "Windows.h"
+#endif
+
+#if defined (__APPLE__) || defined (UNIX)
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <clocale>
+#include <locale>
+#include <vector>
+#include <iconv.h>
+#include <errno.h>
+#endif
 
 #include "strhelper.h"
 
 namespace sunjwbase
 {
+#if defined (WIN32)
+    // Windows convert
 	static std::string _wstrtostr(const std::wstring& wstr, UINT codePage)
 	{
 		// Convert a wstring to an string with specified code page
@@ -30,7 +45,7 @@ namespace sunjwbase
 		delete[] szTo;
 		return strTo;
 	}
-
+    
 	static std::wstring _strtowstr(const std::string& str, UINT codePage)
 	{
 		// Convert an string to a wstring with specified code page
@@ -43,7 +58,43 @@ namespace sunjwbase
 		delete[] wszTo;
 		return wstrTo;
 	}
+#endif
 }
+
+#if defined (__APPLE__) || defined (UNIX)
+std::string sunjwbase::striconv(const std::string& input,
+                                const std::string& to_code,
+                                const std::string& from_code)
+{
+    char* inptr = new char[input.size() + 1];
+    size_t inleft = input.size();
+    size_t outleft = inleft * 4 + 1; // should be large enough
+    char* outptr = new char[outleft];
+    bzero(outptr, outleft);
+    
+    strcpy(inptr, input.c_str());
+    
+    iconv_t cd; // conversion descriptor
+    if ((cd = iconv_open(to_code.c_str(), from_code.c_str())) == (iconv_t) (-1))
+    {
+        iconv_close(cd); // failed clean
+        return input;
+    }
+    
+    char* in = inptr;
+    char* out = outptr;
+    outleft = iconv(cd, &in, &inleft, &out, &outleft);
+    
+    iconv_close(cd);
+    
+    std::string strRet(outptr);
+    
+    delete[] inptr;
+    delete[] outptr;
+    
+    return strRet;
+}
+#endif
 
 /*
  * 将 ascii 编码的 wstring 转换为 utf8 编码的 string
@@ -51,7 +102,12 @@ namespace sunjwbase
 std::string sunjwbase::wstrtostrutf8(const std::wstring& wstr)
 {
     // Convert a Unicode wstring to an ASCII string
+#if defined (WIN32)
     return _wstrtostr(wstr, CP_UTF8);
+#endif
+#if defined (__APPLE__) || defined (UNIX)
+    return striconv(wstrtostr(wstr), "UTF-8", "ASCII");
+#endif
 }
 
 /*
@@ -62,51 +118,78 @@ std::string sunjwbase::wstrtostrutf8(const std::wstring& wstr)
 std::wstring sunjwbase::strtowstrutf8(const std::string& str)
 {
     // Convert an ASCII string to a Unicode wstring
+#if defined (WIN32)
     return _strtowstr(str, CP_UTF8);
+#endif
+#if defined (__APPLE__) || defined (UNIX)
+    return strtowstr(striconv(str, "ASCII", "UTF-8"));
+#endif
 }
 
 std::string sunjwbase::wstrtostr(const std::wstring& wstr)
 {
     // Convert a wstring to an string
+#if defined (WIN32)
     return _wstrtostr(wstr, CP_ACP);
+#endif
+#if defined (__APPLE__) || defined (UNIX)
+	size_t num_chars = wcstombs(NULL, wstr.c_str(), 0);
+	char* char_buf = new char[num_chars + 1];
+	wcstombs(char_buf, wstr.c_str(), wstr.size());
+	std::string str(char_buf);
+	delete[] char_buf;
+    
+	return str;
+#endif
 }
 
 std::wstring sunjwbase::strtowstr(const std::string& str)
 {
     // Convert an string to a wtring
+#if defined (WIN32)
     return _strtowstr(str, CP_ACP);
+#endif
+#if defined (__APPLE__) || defined (UNIX)
+    size_t num_chars = mbstowcs(NULL, str.c_str(), 0);
+	wchar_t* wct_buf = new wchar_t[num_chars + 1];
+	mbstowcs(wct_buf, str.c_str(), str.size());
+	std::wstring wstr(wct_buf, num_chars);
+	delete[] wct_buf;
+    
+	return wstr;
+#endif
 }
 
 std::string sunjwbase::strtrim_right(const std::string& s, const std::string& spaces)
-{ 
-	std::string d(s); 
+{
+	std::string d(s);
 	std::string::size_type i(d.find_last_not_of(spaces));
 	if(i == std::string::npos)
 		return "";
 	else
-		return d.erase(d.find_last_not_of(spaces) + 1); 
+		return d.erase(d.find_last_not_of(spaces) + 1);
 }  // end of trim_right
 
-std::string sunjwbase::strtrim_left(const std::string& s, const std::string& spaces) 
-{ 
-	std::string d(s); 
-	return d.erase(0, s.find_first_not_of(spaces)); 
+std::string sunjwbase::strtrim_left(const std::string& s, const std::string& spaces)
+{
+	std::string d(s);
+	return d.erase(0, s.find_first_not_of(spaces));
 }  // end of trim_left
 
 std::wstring sunjwbase::strtrim_right(const std::wstring& s, const std::wstring& spaces)
 {
-	std::wstring d(s); 
+	std::wstring d(s);
 	std::wstring::size_type i(d.find_last_not_of(spaces));
 	if(i == std::wstring::npos)
 		return L"";
 	else
-		return d.erase(d.find_last_not_of(spaces) + 1); 
+		return d.erase(d.find_last_not_of(spaces) + 1);
 }
 
 std::wstring sunjwbase::strtrim_left(const std::wstring& s, const std::wstring& spaces)
 {
-	std::wstring d(s); 
-	return d.erase(0, s.find_first_not_of(spaces)); 
+	std::wstring d(s);
+	return d.erase(0, s.find_first_not_of(spaces));
 }
 
 std::string sunjwbase::strreplace(const std::string& base, const std::string& src, const std::string& des)
@@ -115,13 +198,13 @@ std::string sunjwbase::strreplace(const std::string& base, const std::string& sr
 	std::string::size_type pos = 0;
 	std::string::size_type srcLen = src.size();
 	std::string::size_type desLen = des.size();
-	pos = ret.find(src, pos); 
+	pos = ret.find(src, pos);
 	while((pos != std::string::npos))
 	{
 		ret.replace(pos, srcLen, des);
 		pos = ret.find(src, pos + desLen);
 	}
-
+    
 	return ret;
 }
 
@@ -131,13 +214,13 @@ std::wstring sunjwbase::strreplace(const std::wstring& base, const std::wstring&
 	std::wstring::size_type pos = 0;
 	std::wstring::size_type srcLen = src.size();
 	std::wstring::size_type desLen = des.size();
-	pos = ret.find(src, pos); 
+	pos = ret.find(src, pos);
 	while((pos != std::wstring::npos))
 	{
 		ret.replace(pos, srcLen, des);
 		pos = ret.find(src, pos + desLen);
 	}
-
+    
 	return ret;
 }
 
@@ -182,3 +265,48 @@ std::string sunjwbase::str_lower(const std::string& str)
 	return ret;
 }
 
+std::string sunjwbase::itostr(int num, int idx /* = 10 */)
+{
+    if(idx > 16)
+        idx = 16;
+    char temp[2] = {0};
+    std::string ret;
+    while(1)
+    {
+        int n = num % idx;
+        if(n < 10)
+            temp[0] = n + '0';
+        else
+            temp[0] = n - 10 + 'A';
+        ret = temp + ret;
+        num /= idx;
+        if(num == 0)
+            break;
+    }
+    
+    return ret;
+}
+
+std::string sunjwbase::strappendformat(std::string& str, const char *format, ...)
+{
+    int size = 100;
+    std::string temp;
+    va_list vl;
+    while (1) {
+        temp.resize(size);
+        va_start(vl, format);
+        int n = vsnprintf((char *)temp.c_str(), size, format, vl);
+        va_end(vl);
+        if (n > -1 && n < size) {
+            temp.resize(n);
+            break;
+        }
+        if (n > -1)
+            size = n + 1; // not large enough
+        else
+            size *= 2;
+    }
+    str.append(temp);
+    
+    return str;
+}
