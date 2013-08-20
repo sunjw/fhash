a260618742f892867373bdd095b9a637f63e63b7
sunjw8888@gmail.com <sunjw8888@gmail.com@9d2edc16-0e2a-0908-9cea-4f64f3160d6d>
2013-08-20 00:38:06 +0000

    去掉 Shell extension 64位的下划线

--- sub-proj/fHashShlExt/fHashShlExt_i.c
+++ sub-proj/fHashShlExt/fHashShlExt_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Mon Aug 19 23:30:14 2013
+/* at Tue Aug 20 08:34:03 2013
  */
 /* Compiler settings for .\fHashShlExt.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
