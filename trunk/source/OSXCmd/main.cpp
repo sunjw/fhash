df61f0119dbde5ede5a34b6b27f9770021375d79
Sun Junwen <sunjw8888@gmail.com>
2015-05-10 01:18:10 +0800

    Test case.

--- trunk/source/OSXCmd/main.cpp
+++ trunk/source/OSXCmd/main.cpp
@@ -7,17 +7,33 @@
 //
 
 #include <stdio.h>
+#include <string>
 
+#include "Global.h"
 #include "OsThread.h"
+#include "HashEngine.h"
 
+using namespace std;
 using namespace sunjwbase;
 
 OsMutex g_mainMtx;
 
 int main(int argc, char *argv[])
 {
+    ThreadData thData;
     
+    thData.threadWorking = false;
+    thData.stop = false;
+    thData.uppercase = false;
     
+    string strPath = "/Users/sunjw/temp/com.microsoft.office.officehub-2.apk";
+    tstring tstrPath = strtotstr(strPath);
+    thData.fullPaths.push_back(tstrPath);
+    thData.nFiles = 1;
+    
+    HashThreadFunc(&thData);
+    
+    printf("DONE\n");
     
     return 0;
 }
