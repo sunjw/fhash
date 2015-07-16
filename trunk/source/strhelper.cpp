90dfa94bfa7140fef72c84f38f93a756e96a4e6e
Sun Junwen <sunjw8888@gmail.com>
2015-07-16 10:14:33 +0800

    Update strhelper.

--- trunk/source/strhelper.cpp
+++ trunk/source/strhelper.cpp
@@ -1,7 +1,7 @@
 /*
  * strhelper implementation file
  * Author: Sun Junwen
- * Version: 2.0
+ * Version: 2.1
  * Provides converting from tstring, string and wstring to each other
  * And provides string's utf8 converting.
  * Provides triming function to string and wstring.
@@ -10,21 +10,23 @@
  */
 #include "stdafx.h"
 
+#include <math.h>
 #include <string>
 
 #if defined (WIN32)
 #include "Windows.h"
 #endif
 
-#if defined (__APPLE__) || defined (UNIX)
-#include <cstdlib>
-#include <cstring>
-#include <iostream>
-#include <clocale>
-#include <locale>
-#include <vector>
+#if defined (__APPLE__) || defined (__unix)
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <locale.h>
 #include <iconv.h>
 #include <errno.h>
+#include <iostream>
+#include <locale>
+#include <vector>
 #endif
 
 #include "strhelper.h"
@@ -32,7 +34,7 @@
 namespace sunjwbase
 {
 #if defined (WIN32)
-    // Windows convert
+	// Windows convert
 	static std::string _wstrtostr(const std::wstring& wstr, UINT codePage)
 	{
 		// Convert a wstring to an string with specified code page
@@ -45,7 +47,7 @@ namespace sunjwbase
 		delete[] szTo;
 		return strTo;
 	}
-    
+	
 	static std::wstring _strtowstr(const std::string& str, UINT codePage)
 	{
 		// Convert an string to a wstring with specified code page
@@ -61,38 +63,38 @@ namespace sunjwbase
 #endif
 }
 
-#if defined (__APPLE__) || defined (UNIX)
+#if defined (__APPLE__) || defined (__unix)
 std::string sunjwbase::striconv(const std::string& input,
-                                const std::string& to_code,
-                                const std::string& from_code)
+								const std::string& to_code,
+								const std::string& from_code)
 {
-    char* inptr = new char[input.size() + 1];
-    size_t inleft = input.size();
-    size_t outleft = inleft * 4 + 1; // should be large enough
-    char* outptr = new char[outleft];
-    bzero(outptr, outleft);
-    
-    strcpy(inptr, input.c_str());
-    
-    iconv_t cd; // conversion descriptor
-    if ((cd = iconv_open(to_code.c_str(), from_code.c_str())) == (iconv_t) (-1))
-    {
-        iconv_close(cd); // failed clean
-        return input;
-    }
-    
-    char* in = inptr;
-    char* out = outptr;
-    outleft = iconv(cd, &in, &inleft, &out, &outleft);
-    
-    iconv_close(cd);
-    
-    std::string strRet(outptr);
-    
-    delete[] inptr;
-    delete[] outptr;
-    
-    return strRet;
+	char* inptr = new char[input.size() + 1];
+	size_t inleft = input.size();
+	size_t outleft = inleft * 4 + 1; // should be large enough
+	char* outptr = new char[outleft];
+	bzero(outptr, outleft);
+	
+	strcpy(inptr, input.c_str());
+	
+	iconv_t cd; // conversion descriptor
+	if ((cd = iconv_open(to_code.c_str(), from_code.c_str())) == (iconv_t) (-1))
+	{
+		iconv_close(cd); // failed clean
+		return input;
+	}
+	
+	char* in = inptr;
+	char* out = outptr;
+	outleft = iconv(cd, &in, &inleft, &out, &outleft);
+	
+	iconv_close(cd);
+	
+	std::string strRet(outptr);
+	
+	delete[] inptr;
+	delete[] outptr;
+	
+	return strRet;
 }
 #endif
 
@@ -101,12 +103,12 @@ std::string sunjwbase::striconv(const std::string& input,
  */
 std::string sunjwbase::wstrtostrutf8(const std::wstring& wstr)
 {
-    // Convert a Unicode wstring to an ASCII string
+	// Convert a Unicode wstring to an ASCII string
 #if defined (WIN32)
-    return _wstrtostr(wstr, CP_UTF8);
+	return _wstrtostr(wstr, CP_UTF8);
 #endif
-#if defined (__APPLE__) || defined (UNIX)
-    return striconv(wstrtostr(wstr), "UTF-8", "ASCII");
+#if defined (__APPLE__) || defined (__unix)
+	return striconv(wstrtostr(wstr), "UTF-8", "ASCII");
 #endif
 }
 
@@ -117,51 +119,91 @@ std::string sunjwbase::wstrtostrutf8(const std::wstring& wstr)
  */
 std::wstring sunjwbase::strtowstrutf8(const std::string& str)
 {
-    // Convert an ASCII string to a Unicode wstring
+	// Convert an ASCII string to a Unicode wstring
 #if defined (WIN32)
-    return _strtowstr(str, CP_UTF8);
+	return _strtowstr(str, CP_UTF8);
 #endif
-#if defined (__APPLE__) || defined (UNIX)
-    return strtowstr(striconv(str, "ASCII", "UTF-8"));
+#if defined (__APPLE__) || defined (__unix)
+	return strtowstr(striconv(str, "ASCII", "UTF-8"));
 #endif
 }
 
 std::string sunjwbase::wstrtostr(const std::wstring& wstr)
 {
-    // Convert a wstring to an string
+	// Convert a wstring to an string
 #if defined (WIN32)
-    return _wstrtostr(wstr, CP_ACP);
+	return _wstrtostr(wstr, CP_ACP);
 #endif
-#if defined (__APPLE__) || defined (UNIX)
-    setlocale(LC_ALL, "zh_CN.UTF-8");
+#if defined (__APPLE__) || defined (__unix)
+	setlocale(LC_ALL, "zh_CN.UTF-8");
 	size_t num_chars = wcstombs(NULL, wstr.c_str(), 0);
 	char* char_buf = new char[num_chars + 1];
 	wcstombs(char_buf, wstr.c_str(), num_chars);
+	char_buf[num_chars] = '\0';
 	std::string str(char_buf);
 	delete[] char_buf;
-    setlocale(LC_ALL, "C");
-    
+	setlocale(LC_ALL, "C");
+	
 	return str;
 #endif
 }
 
 std::wstring sunjwbase::strtowstr(const std::string& str)
 {
-    // Convert an string to a wtring
+	// Convert an string to a wtring
 #if defined (WIN32)
-    return _strtowstr(str, CP_ACP);
+	return _strtowstr(str, CP_ACP);
 #endif
-#if defined (__APPLE__) || defined (UNIX)
-    setlocale(LC_ALL, "zh_CN.UTF-8");
-    size_t num_chars = mbstowcs(NULL, str.c_str(), 0);
+#if defined (__APPLE__) || defined (__unix)
+	setlocale(LC_ALL, "zh_CN.UTF-8");
+	size_t num_chars = mbstowcs(NULL, str.c_str(), 0);
 	wchar_t* wct_buf = new wchar_t[num_chars + 1];
 	mbstowcs(wct_buf, str.c_str(), num_chars);
+	wct_buf[num_chars] = '\0';
 	std::wstring wstr(wct_buf, num_chars);
 	delete[] wct_buf;
-    setlocale(LC_ALL, "C");
-    
+	setlocale(LC_ALL, "C");
+	
 	return wstr;
 #endif
+}
+
+std::string sunjwbase::asciiconvjson(std::string& strJsonUtf16)
+{
+	std::wstring wstrTemp;
+
+	std::string::size_type i = 0;
+	for (; i + 5 < strJsonUtf16.length(); ++i)
+	{
+		if (strJsonUtf16[i] == '\\' &&
+			strJsonUtf16[i + 1] == 'u')
+		{
+			char n[4];
+			n[0] = strJsonUtf16[i + 2];
+			n[1] = strJsonUtf16[i + 3];
+			n[2] = strJsonUtf16[i + 4];
+			n[3] = strJsonUtf16[i + 5];
+			
+			i += 5;
+
+			int utf16 = 0;
+			for (int j = 0; j < 4; ++j)
+			{
+				int num = 0;
+				if (n[j] > 'a')
+					num = n[j] - 'a' + 10;
+				else if (n[j] > '0')
+					num = n[j] - '0';
+
+				utf16 += (int)(num * pow(16.0, 3.0 - j));
+			}
+
+			wchar_t wch = utf16;
+			wstrTemp += wch;
+		}
+	}
+
+	return sunjwbase::wstrtostr(wstrTemp);
 }
 
 std::string sunjwbase::strtrim_right(const std::string& s, const std::string& spaces)
@@ -208,7 +250,7 @@ std::string sunjwbase::strreplace(const std::string& base, const std::string& sr
 		ret.replace(pos, srcLen, des);
 		pos = ret.find(src, pos + desLen);
 	}
-    
+	
 	return ret;
 }
 
@@ -224,7 +266,7 @@ std::wstring sunjwbase::strreplace(const std::wstring& base, const std::wstring&
 		ret.replace(pos, srcLen, des);
 		pos = ret.find(src, pos + desLen);
 	}
-    
+	
 	return ret;
 }
 
@@ -271,52 +313,52 @@ std::string sunjwbase::str_lower(const std::string& str)
 
 std::string sunjwbase::itostr(int num, int idx /* = 10 */)
 {
-    if(idx > 16)
-        idx = 16;
-    char temp[2] = {0};
-    std::string ret;
-    while(1)
-    {
-        int n = num % idx;
-        if(n < 10)
-            temp[0] = n + '0';
-        else
-            temp[0] = n - 10 + 'A';
-        ret = temp + ret;
-        num /= idx;
-        if(num == 0)
-            break;
-    }
-    
-    return ret;
+	if(idx > 16)
+		idx = 16;
+	char temp[2] = {0};
+	std::string ret;
+	while(1)
+	{
+		int n = num % idx;
+		if(n < 10)
+			temp[0] = n + '0';
+		else
+			temp[0] = n - 10 + 'A';
+		ret = temp + ret;
+		num /= idx;
+		if(num == 0)
+			break;
+	}
+	
+	return ret;
 }
 
 std::string sunjwbase::strappendformat(std::string& str, const char *format, ...)
 {
-    int size = 100;
-    std::string temp;
-    va_list vl;
-    while (1) {
-        temp.resize(size);
-        va_start(vl, format);
+	int size = 100;
+	std::string temp;
+	va_list vl;
+	while (1) {
+		temp.resize(size);
+		va_start(vl, format);
 #if defined (WIN32)
 		int n = vsnprintf_s((char *)temp.c_str(), size, size, format, vl);
 #else
-        int n = vsnprintf((char *)temp.c_str(), size, format, vl);
+		int n = vsnprintf((char *)temp.c_str(), size, format, vl);
 #endif
-        va_end(vl);
-        if (n > -1 && n < size) {
-            temp.resize(n);
-            break;
-        }
-        if (n > -1)
-            size = n + 1; // not large enough
-        else
-            size *= 2;
-    }
-    str.append(temp);
-    
-    return str;
+		va_end(vl);
+		if (n > -1 && n < size) {
+			temp.resize(n);
+			break;
+		}
+		if (n > -1)
+			size = n + 1; // not large enough
+		else
+			size *= 2;
+	}
+	str.append(temp);
+	
+	return str;
 }
 
 bool sunjwbase::str_startwith(const std::string& str, const std::string& target)
--- trunk/source/strhelper.h
+++ trunk/source/strhelper.h
@@ -1,7 +1,7 @@
 /*
  * strhelper header file
  * Author: Sun Junwen
- * Version: 2.0
+ * Version: 2.1
  * Provides converting from tstring, string and wstring to each other
  * And provides string's utf8 converting.
  * Provides triming function to string and wstring.
@@ -23,14 +23,12 @@ namespace sunjwbase
 #else
 	typedef std::string tstring;
 #endif
-    
+	
 #if defined (WIN32)
 	typedef __int64 INT64;
 #endif
-#if defined (__APPLE__) || defined (UNIX)
-    typedef long long INT64;
-    typedef tstring::value_type TCHAR;
-    typedef TCHAR _TCHAR;
+#if defined (__APPLE__) || defined (__unix)
+	typedef long long INT64;
 #endif
 
 	// converting part
@@ -44,39 +42,43 @@ namespace sunjwbase
 	 * 2nd, call strtowstr
 	 */
 	std::wstring strtowstrutf8(const std::string& str);
-    
+	
 	std::string wstrtostr(const std::wstring& wstr);
 	std::wstring strtowstr(const std::string& str);
-    
-#if defined (__APPLE__) || defined (UNIX)
-    std::string striconv(const std::string& input,
-                         const std::string& to_code,
-                         const std::string& from_code);
+	
+#if defined (__APPLE__) || defined (__unix)
+	std::string striconv(const std::string& input,
+						 const std::string& to_code,
+						 const std::string& from_code);
 #endif
-    
+
 	// convert native encoded string to utf8 encoded string
 	inline std::string utf8conv(std::string& strAscii)
 	{
 #if defined (WIN32)
-        return wstrtostrutf8(strtowstr(strAscii));
+		return wstrtostrutf8(strtowstr(strAscii));
 #endif
-#if defined (__APPLE__) || defined (UNIX)
-        // current only support GB
-        return striconv(strAscii, "UTF-8", "GBK");
+#if defined (__APPLE__) || defined (__unix)
+		// current only support GB
+		return striconv(strAscii, "UTF-8", "GBK");
 #endif
-    }
+	}
+
 	// convert utf8 encoded string to native encoded string
 	inline std::string asciiconv(std::string& strUtf8)
 	{
 #if defined (WIN32)
-        return wstrtostr(strtowstrutf8(strUtf8));
+		return wstrtostr(strtowstrutf8(strUtf8));
 #endif
-#if defined (__APPLE__) || defined (UNIX)
-        // current only support GB
-        return striconv(strUtf8, "GBK", "UTF-8");
+#if defined (__APPLE__) || defined (__unix)
+		// current only support GB
+		return striconv(strUtf8, "GBK", "UTF-8");
 #endif
-    }
-    
+	}
+
+	// convert JSON utf16 encoded string to native encoded string
+	std::string asciiconvjson(std::string& strJsonUtf16);
+	
 	inline std::string tstrtostr(const tstring& tstr)
 	{
 #if defined(_UNICODE_HELPER)
@@ -85,7 +87,7 @@ namespace sunjwbase
 		return tstr;
 #endif
 	}
-    
+	
 	inline std::wstring tstrtowstr(const tstring& tstr)
 	{
 #if defined(_UNICODE_HELPER)
@@ -94,7 +96,7 @@ namespace sunjwbase
 		return strtowstr(tstr);
 #endif
 	}
-    
+
 	inline sunjwbase::tstring strtotstr(const std::string& str)
 	{
 #if defined(_UNICODE_HELPER)
@@ -103,7 +105,7 @@ namespace sunjwbase
 		return str;
 #endif
 	}
-    
+
 	inline sunjwbase::tstring wstrtotstr(const std::wstring& wstr)
 	{
 #if defined(_UNICODE_HELPER)
@@ -113,37 +115,37 @@ namespace sunjwbase
 #endif
 	}
 	// converting part
-    
+
 	// triming part
 	const std::string SPACES(" \t\r\n");
 	const std::wstring WSPACES(L" \t\r\n");
-    
+
 	// string version
 	std::string strtrim_right(const std::string& s, const std::string& spaces = SPACES);
 	std::string strtrim_left(const std::string& s, const std::string& spaces = SPACES);
 	inline std::string strtrim(const std::string& s, const std::string& spaces = SPACES)
 	{ return strtrim_left(strtrim_right(s, spaces), spaces); }
-    
+
 	// wstring version
 	std::wstring strtrim_right(const std::wstring& s, const std::wstring& spaces = WSPACES);
 	std::wstring strtrim_left(const std::wstring& s, const std::wstring& spaces = WSPACES);
 	inline std::wstring strtrim(const std::wstring& s, const std::wstring& spaces = WSPACES)
 	{ return strtrim_left(strtrim_right(s, spaces), spaces); }
 	// triming part
-    
+
 	// replacing part
 	std::string strreplace(const std::string& base, const std::string& src, const std::string& des);
 	std::wstring strreplace(const std::wstring& base, const std::wstring& src, const std::wstring& des);
 	// replacing part
-    
+
 	// fixing
 	std::string fixnewline(const std::string& str);
 	// fixing
-    
+
 	// upper and lower
 	std::string str_upper(const std::string& str);
 	std::string str_lower(const std::string& str);
-    
+
 	// searching part
 	// case insensitive equal for ci_strfind
 	// templated version of ci_equal so it could work with both char and wchar_t
@@ -152,7 +154,7 @@ namespace sunjwbase
 	{
 		strequal_ci( const std::locale& loc ):m_loc(loc)
 		{}
-        
+		
 		bool operator()(charT ch1, charT ch2)
 		{
 			return std::toupper(ch1, m_loc) == std::toupper(ch2, m_loc);
@@ -160,25 +162,25 @@ namespace sunjwbase
 	private:
 		const std::locale& m_loc;
 	};
-    
+
 	// case insensitive search
 	template<typename T>
 	INT64 strfind_ci(const T& str, const T& substr, const std::locale& loc = std::locale())
 	{
 		typename T::const_iterator it = std::search(str.begin(), str.end(),
-                                           substr.begin(), substr.end(),
-                                           strequal_ci<typename T::value_type>(loc));
+										   substr.begin(), substr.end(),
+										   strequal_ci<typename T::value_type>(loc));
 		if (it != str.end()) 
 			return it - str.begin();
 		else 
 			return -1; // not found
 	}
-    
-    // itostr
-    std::string itostr(int num, int idx = 10);
+	
+	// itostr
+	std::string itostr(int num, int idx = 10);
 
-    // format append for std::string
-    std::string strappendformat(std::string& str, const char *format, ...);
+	// format append for std::string
+	std::string strappendformat(std::string& str, const char *format, ...);
 
 	// start with & end with
 	bool str_startwith(const std::string& str, const std::string& target);
