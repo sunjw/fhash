30dde5ab8019d25465e0f207cb90c4d808902dc1
Sun Junwen <sunjw8888@gmail.com>
2015-07-20 10:41:29 +0800

    Update MacUtils.

--- trunk/source/MacUtils.h
+++ trunk/source/MacUtils.h
@@ -9,5 +9,13 @@
 #ifndef _MAC_UTILS_H_
 #define _MAC_UTILS_H_
 
+#include <string>
+#import <Foundation/Foundation.h>
+
+namespace MacUtils
+{
+    std::string ConvertNSStringToUTF8String(NSString *nsstr);
+
+}
 
 #endif
