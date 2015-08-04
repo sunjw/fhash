fda480763f375f23090ba2759364d1b723680078
Sun Junwen <sunjw8888@gmail.com>
2015-08-04 17:54:34 +0800

    Add AboutWindowController and AboutViewController.

---
+++ trunk/source/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  fHashMacUI
+//
+//  Created by Sun Junwen on 15/8/4.
+//  Copyright (c) 2015年 Sun Junwen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AboutViewController : NSViewController
+
+@end
---
+++ trunk/source/AboutViewController.m
@@ -0,0 +1,22 @@
+//
+//  AboutViewController.m
+//  fHashMacUI
+//
+//  Created by Sun Junwen on 15/8/4.
+//  Copyright (c) 2015年 Sun Junwen. All rights reserved.
+//
+
+#import "AboutViewController.h"
+
+@interface AboutViewController ()
+
+@end
+
+@implementation AboutViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
