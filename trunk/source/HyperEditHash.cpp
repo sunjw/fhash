eeae32adace1fe909f08fdba1ae487de3f8e7073
Sun Junwen <sunjw8888@gmail.com>
2016-07-09 07:59:10 +0800

    Cleanup spaces.

--- trunk/source/HyperEditHash.cpp
+++ trunk/source/HyperEditHash.cpp
@@ -39,38 +39,38 @@ BOOL CHyperEditHash::PreTranslateMessage(MSG* pMsg)
                 this->SetSel(0, -1);  
                 return true;  
             }  
-      
+
             // ¿½±´  
             if( pMsg->wParam == 'C' || pMsg->wParam == 'c')  
             {  
                 this->Copy();  
                 return true;  
             }  
-   
+
             // ¼ôÇÐ  
             if( pMsg->wParam == 'X' || pMsg->wParam == 'x')  
             {  
                 this->Cut();  
                 return true;  
             }  
-      
+
             // Õ³Ìù  
             if( pMsg->wParam == 'V' || pMsg->wParam == 'v')  
             {  
                 this->Paste();  
                 return true;  
             }  
-      
+
             // Õ³Ìù  
             if( pMsg->wParam == 'Z' || pMsg->wParam == 'z')  
             {  
                 this->Undo();  
                 return true;  
              }  
-      
+
         }  
     }  
-      
+
     return CHyperEditHash_BASE_CLASS::PreTranslateMessage(pMsg);  
  }  
 
