68fa55fd41f9fc73e5edf256ac6d8bb0d8547106
Sun Junwen <sunjw8888@gmail.com>
2020-11-21 22:40:43 +0800

    win: Some code clean.

--- trunk/source/WinMFC/HyperEditHash.cpp
+++ trunk/source/WinMFC/HyperEditHash.cpp
@@ -38,35 +38,35 @@ BOOL CHyperEditHash::PreTranslateMessage(MSG* pMsg)
 		if(::GetFocus() == GetSafeHwnd() && (GetKeyState( VK_CONTROL) & 0xFF00 ) == 0xFF00)   
         {
             // È«Ñ¡
-            if( pMsg->wParam == 'A' || pMsg->wParam == 'a')  
+            if( pMsg->wParam == 'A' || pMsg->wParam == 'a')
             {
                 this->SetSel(0, -1);
                 return true;
             }
 
             // ¿½±´
-            if( pMsg->wParam == 'C' || pMsg->wParam == 'c')  
+            if( pMsg->wParam == 'C' || pMsg->wParam == 'c')
             {
                 this->Copy();
                 return true;
 			}
 
             // ¼ôÇĞ
-            if( pMsg->wParam == 'X' || pMsg->wParam == 'x')  
+            if( pMsg->wParam == 'X' || pMsg->wParam == 'x')
             {
                 this->Cut();
                 return true;
 			}
 
             // Õ³Ìù
-            if( pMsg->wParam == 'V' || pMsg->wParam == 'v')  
+            if( pMsg->wParam == 'V' || pMsg->wParam == 'v')
             {
                 this->Paste();
                 return true;
             }
 
             // ³·Ïú
-            if( pMsg->wParam == 'Z' || pMsg->wParam == 'z')  
+            if( pMsg->wParam == 'Z' || pMsg->wParam == 'z')
             {
                 this->Undo();
                 return true;
