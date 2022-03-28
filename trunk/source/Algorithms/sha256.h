d88bec85767a905e9c065c8850a54ca1d4abee1e
Sun Junwen <sunjw8888@gmail.com>
2022-03-28 10:48:28 +0800

    Some tweak.

--- trunk/source/Algorithms/sha256.h
+++ trunk/source/Algorithms/sha256.h
@@ -43,19 +43,11 @@
 
 #include <string>
 
-typedef signed char int8_t;
-typedef unsigned char uint8_t;
-
-typedef signed short int16_t;
-typedef unsigned short uint16_t;
-
-typedef signed int int32_t;
-typedef unsigned int uint32_t;
-
-typedef signed long long int64_t;
-typedef unsigned long long uint64_t;
-
-
+#if defined (WIN32)
+#include "WinMFC/stdint.h"
+#else
+#include <stdint.h>
+#endif
 
 #define SHA256_DIGEST_SIZE 32
 #define SHA256_DATA_SIZE 64
