942a5d7d8666be081677a4353eb46b999bbe2c08
Sun Junwen <sunjw8888@gmail.com>
2022-06-13 14:06:27 +0800

    uwp: Init UIBridgeUwp.

---
+++ sub-proj/fHashWinRtBridge/UIBridgeUwp.cpp
@@ -0,0 +1,73 @@
+#include "stdafx.h"
+
+#include "UIBridgeUwp.h"
+#include "Common/Global.h"
+
+UIBridgeUwp::UIBridgeUwp()
+{
+}
+
+UIBridgeUwp::~UIBridgeUwp()
+{
+}
+
+void UIBridgeUwp::lockData()
+{
+}
+
+void UIBridgeUwp::unlockData()
+{
+}
+
+void UIBridgeUwp::preparingCalc()
+{
+}
+
+void UIBridgeUwp::removePreparingCalc()
+{
+}
+
+void UIBridgeUwp::calcStop()
+{
+}
+
+void UIBridgeUwp::calcFinish()
+{
+}
+
+void UIBridgeUwp::showFileName(const ResultData& result)
+{
+}
+
+void UIBridgeUwp::showFileMeta(const ResultData& result)
+{
+}
+
+void UIBridgeUwp::showFileHash(const ResultData& result, bool uppercase)
+{
+}
+
+void UIBridgeUwp::showFileErr(const ResultData& result)
+{
+}
+
+int UIBridgeUwp::getProgMax()
+{
+	return 100;
+}
+
+void UIBridgeUwp::updateProg(int value)
+{
+}
+
+void UIBridgeUwp::updateProgWhole(int value)
+{
+}
+
+void UIBridgeUwp::fileCalcFinish()
+{
+}
+
+void UIBridgeUwp::fileFinish()
+{
+}
---
+++ sub-proj/fHashWinRtBridge/UIBridgeUwp.h
@@ -0,0 +1,36 @@
+#ifndef _UI_BRIDGE_MFC_
+#define _UI_BRIDGE_MFC_
+
+#include "Common/UIBridgeBase.h"
+#include "Common/Global.h"
+
+class UIBridgeUwp: public UIBridgeBase
+{
+public:
+	UIBridgeUwp();
+	virtual ~UIBridgeUwp();
+
+	virtual void lockData();
+	virtual void unlockData();
+
+	virtual void preparingCalc();
+	virtual void removePreparingCalc();
+	virtual void calcStop();
+	virtual void calcFinish();
+
+	virtual void showFileName(const ResultData& result);
+	virtual void showFileMeta(const ResultData& result);
+	virtual void showFileHash(const ResultData& result, bool uppercase);
+	virtual void showFileErr(const ResultData& result);
+
+	virtual int getProgMax();
+	virtual void updateProg(int value);
+	virtual void updateProgWhole(int value);
+
+	virtual void fileCalcFinish();
+	virtual void fileFinish();
+
+private:
+};
+
+#endif
