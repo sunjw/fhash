c25a245b3ffa29107da834fc5022bbc2504d284a
Sun Junwen <sunjw8888@gmail.com>
2022-06-13 14:21:23 +0800

    uwp: Init UIBridgeDelegate.

---
+++ sub-proj/fHashWinRtBridge/UIBridgeDelegate.cpp
@@ -0,0 +1,10 @@
+#include "stdafx.h"
+#include "UIBridgeDelegate.h"
+
+using namespace Platform;
+using namespace FilesHashUwp;
+
+UIBridgeDelegate::UIBridgeDelegate()
+{
+
+}
---
+++ sub-proj/fHashWinRtBridge/UIBridgeDelegate.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace FilesHashUwp
+{
+	public delegate void OnHelloHandler(Platform::String^ strHello);
+
+    public ref class UIBridgeDelegate sealed
+    {
+    public:
+        UIBridgeDelegate();
+
+        event OnHelloHandler^ OnHelloHandler;
+    };
+}
