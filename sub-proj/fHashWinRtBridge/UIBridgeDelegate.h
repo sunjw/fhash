f30f0705ba894a25df4bb376f8edfa087a50541d
Sun Junwen <sunjw8888@gmail.com>
2022-06-13 15:07:32 +0800

    uwp: Implement UIBridgeDelegate.

--- sub-proj/fHashWinRtBridge/UIBridgeDelegate.h
+++ sub-proj/fHashWinRtBridge/UIBridgeDelegate.h
@@ -1,14 +1,30 @@
 #pragma once
 
+#include "ResultDataNet.h"
+
 namespace FilesHashUwp
 {
-	public delegate void OnHelloHandler(Platform::String^ strHello);
+	public delegate void CalcEventHandler();
+	public delegate void ResultEventHandler(ResultDataNet^);
+	public delegate void ResultHashEventHandler(ResultDataNet^, Platform::Boolean);
+	public delegate void CalcProgEventHandler(int32);
 
 	public ref class UIBridgeDelegate sealed
 	{
 	public:
 		UIBridgeDelegate();
 
-		event OnHelloHandler^ OnHelloHandler;
+		event CalcEventHandler^ PreparingCalcHandler;
+		event CalcEventHandler^ RemovePreparingCalcHandler;
+		event CalcEventHandler^ CalcStopHandler;
+		event CalcEventHandler^ CalcFinishHandler;
+
+		event ResultEventHandler^ ShowFileNameHandler;
+		event ResultEventHandler^ ShowFileMetaHandler;
+		event ResultHashEventHandler^ ShowFileHashHandler;
+		event ResultEventHandler^ ShowFileErrHandler;
+
+		event CalcProgEventHandler^ UpdateProgHandler;
+		event CalcProgEventHandler^ UpdateProgWholeHandler;
 	};
 }
