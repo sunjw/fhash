097a4404747b4639f4c49d1a6a6460b90ae7c360
Sun Junwen <sunjw8888@gmail.com>
2022-06-15 13:43:08 +0800

    uwp: Tweak string in fHashUwpShellExt Debug build.

--- sub-proj/fHashUwpShellExt/UwpShellExtStringsBase.cpp
+++ sub-proj/fHashUwpShellExt/UwpShellExtStringsBase.cpp
@@ -5,5 +5,9 @@
 UwpShlExtStringsBase::UwpShlExtStringsBase()
 {
 	// Shell ext
+#if defined (_DEBUG)
+	m_stringsMap[_T("SHELL_EXT_DISPLAY_NAME")] = _T("Hash with fHash DEV");
+#else
 	m_stringsMap[_T("SHELL_EXT_DISPLAY_NAME")] = _T("Hash with fHash");
+#endif
 }
