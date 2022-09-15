79feadbac37124b0084e5d0d7c463c595602a965
Sun Junwen <sunjw8888@gmail.com>
2022-09-15 11:20:50 +0800

    macos: Tweak about dialog.

--- trunk/source/OSXUI/AboutWindowController.mm
+++ trunk/source/OSXUI/AboutWindowController.mm
@@ -21,7 +21,7 @@
     
     // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
     self.window.title = GetNSStringFromResByKey(ABOUTDLG_TITLE);
-    self.window.backgroundColor = [NSColor textBackgroundColor];
+    //self.window.backgroundColor = [NSColor textBackgroundColor];
     
 }
 
