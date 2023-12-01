51cbb4f6bd2cce43e32335fe4093ec3ce7f3c965
Sun Junwen <sunjw8888@gmail.com>
2023-12-01 12:41:49 +0800

    macos: Implement ESSProgressBarAnimation in swift.

--- trunk/source/OSXUI/ESSProgressIndicator.swift
+++ trunk/source/OSXUI/ESSProgressIndicator.swift
@@ -5,46 +5,77 @@
 //  Created by Sun Junwen on 2023/12/1.
 //  Copyright © 2023 Sun Junwen. All rights reserved.
 //
+//
+//  ESSProgressIndicator.m
+//  Briefly
+//
+//  Created by Matthias Gansrigler on 06.05.2015.
+//  Copyright (c) 2015 Eternal Storms Software. All rights reserved.
+//
+//  Edited by Sun Junwen on 2015/08/15.
+//  Swift version created by Sun Junwen on 2023/12/1.
+//
 import Cocoa
 
-class ESSProgressBarAnimation: NSAnimation {
+private class ESSProgressBarAnimation: NSAnimation {
     var progInd: ESSProgressIndicator?
-    var initValue = 0.0
-    var newValue = 0.0
-
-    init(progressBar ind: NSProgressIndicator?, ewDoubleValue val: Double) {
-        super.init(duration: 0.3, animationCurve: .linear)
-        progInd = ind
-        initValue = (progInd as? NSNumber)?.doubleValue ?? 0.0
-        newValue = val
-        animationBlockingMode = .nonblockingThreaded
-
-        return nil
-    }
+    var initValue: Double = 0.0
+    var newValue: Double = 0.0
 
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
 
-    func setCurrentProgress(_ currentProgress: NSAnimation.Progress) {
-        super.currentProgress = currentProgress
+    init(progressBar ind: ESSProgressIndicator?, newDoubleValue val: Double) {
+        super.init(duration: 0.3, animationCurve: .linear)
+        progInd = ind
+        initValue = progInd?.doubleValue ?? 0.0
+        newValue = val
+        animationBlockingMode = .nonblockingThreaded
+    }
 
-        let delta = newValue - progInd.doubleValue
+    override var currentProgress: NSAnimation.Progress {
+        didSet {
+            let delta = newValue - (progInd?.doubleValue ?? 0.0)
 
-        if !progInd.animateDisabled && (delta * (newValue - initValue)) > 0 {
-            let newDoubleValue = progInd.doubleValue + (delta * currentValue)
-            progInd.doubleValue = newDoubleValue
+            if !(progInd?.animateDisabled ?? true) &&
+                (delta * (newValue - initValue)) > 0 {
+                let newDoubleValue = progInd?.doubleValue ?? 0.0 + (delta * Double(currentValue))
+                progInd?.doubleValue = newDoubleValue
+            }
         }
     }
 }
 
+private var sAnim: ESSProgressBarAnimation?
+
 class ESSProgressIndicator: NSProgressIndicator {
-    var toValue = 0.0
+    var toValue:Double = 0.0
     var animateDisabled = false
 
-    func jump(toDoubleValue val: Double) {
+    func cleanOldAnimation() {
+        if sAnim != nil {
+            let oldToValue = sAnim?.newValue ?? 0.0
+            sAnim?.stop()
+            sAnim = nil
+            self.doubleValue = oldToValue
+        }
     }
 
-    func animate(toDoubleValue val: Double) {
+    func jumptoDoubleValue(val: Double) {
+        cleanOldAnimation()
+        animateDisabled = true
+        toValue = val
+        super.doubleValue = self.toValue
+    }
+
+    func animateToDoubleValue(val: Double) {
+        cleanOldAnimation()
+        animateDisabled = false
+        toValue = val
+        sAnim = ESSProgressBarAnimation(
+            progressBar: self,
+            newDoubleValue: toValue)
+        sAnim?.start()
     }
 }
