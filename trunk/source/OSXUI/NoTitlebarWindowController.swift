534b6a4e268c55f3d1a02da9aa17db22d9ce3f33
Sun Junwen <sunjw8888@gmail.com>
2023-12-01 16:33:44 +0800

    macos: Migrate NoTitlebarWindowController to swift.

---
+++ trunk/source/OSXUI/NoTitlebarWindowController.swift
@@ -0,0 +1,21 @@
+//
+//  NoTitlebarWindowController.swift
+//  fHash
+//
+//  Created by Sun Junwen on 2023/12/1.
+//  Copyright © 2023 Sun Junwen. All rights reserved.
+//
+
+import Cocoa
+
+@objc class NoTitlebarWindowController: NSWindowController {
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+        window?.titleVisibility = .hidden
+        window?.titlebarAppearsTransparent = true
+        window?.styleMask.insert(.fullSizeContentView)
+        window?.isMovableByWindowBackground = true
+    }
+}
