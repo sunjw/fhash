5cab69fa9c901d93d001bdb38bf04445b2d1b5fa
Sun Junwen <sunjw8888@gmail.com>
2023-12-01 16:42:10 +0800

    macos: Migrate MainWindowController to swift.

---
+++ trunk/source/OSXUI/MainWindowController.swift
@@ -0,0 +1,34 @@
+//
+//  MainWindowController.swift
+//  fHash
+//
+//  Created by Sun Junwen on 2023/12/1.
+//  Copyright © 2023 Sun Junwen. All rights reserved.
+//
+
+import Cocoa
+
+@objc(MainWindowController) class MainWindowController: NoTitlebarWindowController, NSWindowDelegate {
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+        window?.delegate = self
+    }
+
+    func getMainViewController() -> MainViewController? {
+        var mainViewController: MainViewController?
+        mainViewController = window?.contentViewController as? MainViewController
+        return mainViewController
+    }
+
+    func windowShouldClose(_ sender: NSWindow) -> Bool {
+        let mainViewController = getMainViewController()
+
+        if mainViewController?.isCalculating() ?? false {
+            mainViewController?.stopHashCalc(true)
+        }
+
+        return !(mainViewController?.isCalculating())!
+    }
+}
