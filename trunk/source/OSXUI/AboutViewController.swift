b8ad7cdce591024afa4b2e62a15f4e5c66cc91e8
Sun Junwen <sunjw8888@gmail.com>
2023-12-05 17:29:15 +0800

    macos: Migrate AboutViewController to swift.

---
+++ trunk/source/OSXUI/AboutViewController.swift
@@ -0,0 +1,68 @@
+//
+//  AboutViewController.swift
+//  fHash
+//
+//  Created by Sun Junwen on 2023/12/5.
+//  Copyright © 2023 Sun Junwen. All rights reserved.
+//
+
+import Cocoa
+
+class AboutViewController: NSViewController {
+    @IBOutlet weak var iconImageView: NSImageView!
+    @IBOutlet weak var infoTextField: NSTextField!
+    @IBOutlet weak var homePageLinkTextField: HyperlinkTextField!
+    @IBOutlet weak var closeButton: NSButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do view setup here.
+        iconImageView.image = NSApp.applicationIconImage
+
+        let strAppVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+        let strAppBundleVersion = Bundle.main.infoDictionary?["CFBundleVersion"] as? String
+
+        var strAboutInfo = ""
+        strAboutInfo += MacSwiftUtils.GetStringFromRes("ABOUTDLG_INFO_TITLE")
+        strAboutInfo += " "
+        strAboutInfo += (strAppVersion ?? "")
+        strAboutInfo += " ("
+        strAboutInfo += (strAppBundleVersion ?? "")
+        strAboutInfo += ") Swift"
+        strAboutInfo += "\n"
+        strAboutInfo += MacSwiftUtils.GetStringFromRes("ABOUTDLG_INFO_RIGHT")
+        strAboutInfo += "\n"
+        strAboutInfo += "\n"
+        strAboutInfo += MacSwiftUtils.GetStringFromRes("ABOUTDLG_INFO_MD5")
+        strAboutInfo += "\n"
+        strAboutInfo += MacSwiftUtils.GetStringFromRes("ABOUTDLG_INFO_SHA256")
+        strAboutInfo += "\n"
+        strAboutInfo += MacSwiftUtils.GetStringFromRes("ABOUTDLG_INFO_SHA512")
+        infoTextField.stringValue = strAboutInfo
+
+        // Set homepage.
+        var strLinkText = MacSwiftUtils.GetStringFromRes("ABOUTDLG_PROJECT_SITE")
+        strLinkText = strLinkText.replacingOccurrences(of: "<a>", with: "")
+        strLinkText = strLinkText.replacingOccurrences(of:"</a>", with: "")
+        let url = URL(string: MacSwiftUtils.GetStringFromRes("ABOUTDLG_PROJECT_URL"))!
+        let hyperlinkString = NSMutableAttributedString(
+            string: strLinkText)
+        hyperlinkString.beginEditing()
+        hyperlinkString.addAttribute(
+            .link,
+            value: url,
+            range: NSRange(location: 0, length: hyperlinkString.length))
+        hyperlinkString.endEditing()
+
+        homePageLinkTextField.attributedStringValue = hyperlinkString
+
+        closeButton.keyEquivalent = "\r"
+        closeButton.title = MacSwiftUtils.GetStringFromRes("BUTTON_OK")
+    }
+
+    @IBAction func closeButtonClicked(_ sender: NSButton) {
+        view.window?.close()
+    }
+}
+
