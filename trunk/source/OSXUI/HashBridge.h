117a3c57b959e359dbbb800eefc892f3b8025f4e
Sun Junwen <sunjw8888@gmail.com>
2023-12-07 15:50:14 +0800

    macos: Init HashBridge.

---
+++ trunk/source/OSXUI/HashBridge.h
@@ -0,0 +1,13 @@
+//
+//  HashBridge.h
+//  fHashMacUI
+//
+//  Created by Sun Junwen on 2023/12/7.
+//  Copyright © 2023 Sun Junwen. All rights reserved.
+//
+
+#ifndef HashBridge_h
+#define HashBridge_h
+
+
+#endif /* HashBridge_h */
---
+++ trunk/source/OSXUI/HashBridge.mm
@@ -0,0 +1,9 @@
+//
+//  HashBridge.mm
+//  fHash
+//
+//  Created by Sun Junwen on 2023/12/7.
+//  Copyright © 2023 Sun Junwen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
