9bbd065acedb0362e4329a70bdfd3ea2be9c9cfa
Sun Junwen <sunjw8888@gmail.com>
2023-12-07 09:49:29 +0800

    macos: Init UIBridgeMacSwift.

---
+++ trunk/source/OSXUI/UIBridgeMacSwift.h
@@ -0,0 +1,13 @@
+//
+//  UIBridgeMacSwift.h
+//  fHashMacUI
+//
+//  Created by Sun Junwen on 2023/12/7.
+//  Copyright © 2023 Sun Junwen. All rights reserved.
+//
+
+#ifndef UIBridgeMacSwift_h
+#define UIBridgeMacSwift_h
+
+
+#endif /* UIBridgeMacSwift_h */
