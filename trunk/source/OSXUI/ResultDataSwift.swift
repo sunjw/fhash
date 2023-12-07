3475f692f3c525e7f6ed232cbbdfb9668c40138d
Sun Junwen <sunjw8888@gmail.com>
2023-12-07 10:58:47 +0800

    macos: Implement ResultDataSwift.

--- trunk/source/OSXUI/ResultDataSwift.swift
+++ trunk/source/OSXUI/ResultDataSwift.swift
@@ -9,5 +9,14 @@
 import Foundation
 
 @objc(ResultDataSwift) class ResultDataSwift: NSObject {
-
+    @objc var state:Int = 0 // State
+    @objc var strPath:String = "" // Path
+    @objc var ulSize:UInt64 = 0 // Size
+    @objc var strMDate:String = "" // Modified date
+    @objc var strVersion:String = "" // Version
+    @objc var strMD5:String = "" // MD5
+    @objc var strSHA1:String = "" // SHA1
+    @objc var strSHA256:String = "" // SHA256
+    @objc var strSHA512:String = "" // SHA512
+    @objc var strError:String = "" // Error string
 }
