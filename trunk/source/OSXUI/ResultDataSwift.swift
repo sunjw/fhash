d62ba4adb054270b0acf60c56bb881e9060fc0fc
Sun Junwen <sunjw8888@gmail.com>
2023-12-07 10:49:33 +0800

    macos: Init ResultDataSwift.

---
+++ trunk/source/OSXUI/ResultDataSwift.swift
@@ -0,0 +1,13 @@
+//
+//  ResultDataSwift.swift
+//  fHash
+//
+//  Created by Sun Junwen on 2023/12/7.
+//  Copyright © 2023 Sun Junwen. All rights reserved.
+//
+
+import Foundation
+
+@objc(ResultDataSwift) class ResultDataSwift: NSObject {
+
+}
