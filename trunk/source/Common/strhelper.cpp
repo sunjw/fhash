335eba0e585ffba7323319d3af0de784b31ef52a
Sun Junwen <sunjw8888@gmail.com>
2023-12-15 13:45:24 +0800

    common: Update strhelper.

--- trunk/source/Common/strhelper.cpp
+++ trunk/source/Common/strhelper.cpp
@@ -1,7 +1,7 @@
 /*
  * strhelper implementation file
  * Author: Sun Junwen
- * Version: 2.2.1
+ * Version: 2.2.2
  * Provides converting from tstring, string and wstring to each other
  * And provides string's utf8 converting.
  * Provides triming function to string and wstring.
