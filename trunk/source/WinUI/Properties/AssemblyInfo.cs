241a32f3fbc282231798dd974e13997a9e8d8ea5
Sun Junwen <sunjw8888@gmail.com>
2024-08-19 18:56:28 +0800

    winui: Add AssemblyInfo.

---
+++ trunk/source/WinUI/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+// 有关程序集的一般信息由以下
+// 控制。更改这些特性值可修改
+// 与程序集关联的信息。
+[assembly: AssemblyTitle("fHashWUI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SUN Junwen")]
+[assembly: AssemblyProduct("fHashWUI")]
+[assembly: AssemblyCopyright("Copyright © SUN Junwen 2024-")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: TargetPlatformAttribute("Windows10.0.22000.0")]
+[assembly: SupportedOSPlatformAttribute("Windows10.0.18362.0")]
+
+// 程序集的版本信息由下列四个值组成: 
+//
+//      主版本
+//      次版本
+//      生成号
+//      修订号
+//
+//可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
+//通过使用 "*"，如下所示:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("3.9.0.0")]
+[assembly: AssemblyFileVersion("3.9.0.0")]
+[assembly: ComVisible(false)]
+
