a94a9a82eb55df8536d53b7af08cb073df3b96ab
Sun Junwen <sunjw8888@gmail.com>
2024-08-24 17:07:25 +0800

    winui: Implement AdvTaskbarHelper in fHashClrBridge.

--- sub-proj/fHashClrBridge/AdvTaskbarHelper.cpp
+++ sub-proj/fHashClrBridge/AdvTaskbarHelper.cpp
@@ -1,3 +1,12 @@
 #include "stdafx.h"
-
 #include "AdvTaskbarHelper.h"
+
+using namespace SunJWBase;
+
+AdvTaskbarHelper::AdvTaskbarHelper(HWND hWnd):
+	m_hWnd(hWnd), m_pTaskbarList3(NULL)
+{
+	ITaskbarList3 *pTaskbarListTemp = NULL;
+	if (SUCCEEDED(WindowsComm::InitTaskbarList3(&pTaskbarListTemp)))
+		m_pTaskbarList3 = pTaskbarListTemp;
+}
--- sub-proj/fHashClrBridge/AdvTaskbarHelper.h
+++ sub-proj/fHashClrBridge/AdvTaskbarHelper.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "WinCommon/AdvTaskbar.h"
+
+namespace SunJWBase
+{
+	public ref class AdvTaskbarHelper sealed
+	{
+	public:
+		AdvTaskbarHelper(HWND hWnd);
+
+	private:
+		HWND m_hWnd;
+		ITaskbarList3 *m_pTaskbarList3;
+	};
+}
