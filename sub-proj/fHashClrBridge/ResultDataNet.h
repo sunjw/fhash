b23fbf78909e7711f7db8904cfa6c4c240d732f3
Sun Junwen <sunjw8888@gmail.com>
2024-08-24 17:41:22 +0800

    winui: Implement ResultDataNet.

---
+++ sub-proj/fHashClrBridge/ResultDataNet.h
@@ -0,0 +1,28 @@
+#pragma once
+
+namespace FilesHashUwp
+{
+	public enum class ResultStateNet
+	{
+		ResultNone = 0,
+		ResultPath,
+		ResultMeta,
+		ResultAll,
+		ResultError
+	};
+
+	public value struct ResultDataNet
+	{
+	public:
+		ResultStateNet EnumState; // State
+		System::String^ Path; // Path
+		System::UInt64 Size; // Size
+		System::String^ ModifiedDate; // Modified date
+		System::String^ Version; // Version
+		System::String^ MD5; // MD5
+		System::String^ SHA1; // SHA1
+		System::String^ SHA256; // SHA256
+		System::String^ SHA512; // SHA512
+		System::String^ Error; // Error string
+	};
+}
