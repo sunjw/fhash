0059581dc5d964ed7427e460931d461cee36671c
Sun Junwen <sunjw8888@gmail.com>
2024-08-26 10:30:38 +0800

    winui: Add UIBridgeDelegate to fHashClrBridge.

---
+++ sub-proj/fHashClrBridge/UIBridgeDelegate.cpp
@@ -0,0 +1,59 @@
+#include "stdafx.h"
+#include "UIBridgeDelegate.h"
+
+using namespace System;
+using namespace FilesHashWUI;
+
+UIBridgeDelegate::UIBridgeDelegate()
+{
+}
+
+Int32 UIBridgeDelegate::GetProgMax()
+{
+	return 100;
+}
+
+void UIBridgeDelegate::PreparingCalc()
+{
+	PreparingCalcHandler();
+}
+
+void UIBridgeDelegate::RemovePreparingCalc()
+{
+	RemovePreparingCalcHandler();
+}
+
+void UIBridgeDelegate::CalcStop()
+{
+	CalcStopHandler();
+}
+
+void UIBridgeDelegate::CalcFinish()
+{
+	CalcFinishHandler();
+}
+
+void UIBridgeDelegate::ShowFileName(ResultDataNet resultDataNet)
+{
+	ShowFileNameHandler(resultDataNet);
+}
+
+void UIBridgeDelegate::ShowFileMeta(ResultDataNet resultDataNet)
+{
+	ShowFileMetaHandler(resultDataNet);
+}
+
+void UIBridgeDelegate::ShowFileHash(ResultDataNet resultDataNet, bool uppercase)
+{
+	ShowFileHashHandler(resultDataNet, uppercase);
+}
+
+void UIBridgeDelegate::ShowFileErr(ResultDataNet resultDataNet)
+{
+	ShowFileErrHandler(resultDataNet);
+}
+
+void UIBridgeDelegate::UpdateProgWhole(Int32 value)
+{
+	UpdateProgWholeHandler(value);
+}
---
+++ sub-proj/fHashClrBridge/UIBridgeDelegate.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "ResultDataNet.h"
+
+namespace FilesHashWUI
+{
+	public delegate void CalcEventHandler();
+	public delegate void ResultEventHandler(ResultDataNet);
+	public delegate void ResultHashEventHandler(ResultDataNet, bool);
+	public delegate void CalcProgEventHandler(System::Int32);
+
+	public ref class UIBridgeDelegate sealed
+	{
+	public:
+		UIBridgeDelegate();
+
+		System::Int32 GetProgMax();
+
+		void PreparingCalc();
+		void RemovePreparingCalc();
+		void CalcStop();
+		void CalcFinish();
+
+		void ShowFileName(ResultDataNet resultDataNet);
+		void ShowFileMeta(ResultDataNet resultDataNet);
+		void ShowFileHash(ResultDataNet resultDataNet, bool uppercase);
+		void ShowFileErr(ResultDataNet resultDataNet);
+
+		void UpdateProgWhole(System::Int32 value);
+
+		event CalcEventHandler^ PreparingCalcHandler;
+		event CalcEventHandler^ RemovePreparingCalcHandler;
+		event CalcEventHandler^ CalcStopHandler;
+		event CalcEventHandler^ CalcFinishHandler;
+
+		event ResultEventHandler^ ShowFileNameHandler;
+		event ResultEventHandler^ ShowFileMetaHandler;
+		event ResultHashEventHandler^ ShowFileHashHandler;
+		event ResultEventHandler^ ShowFileErrHandler;
+
+		event CalcProgEventHandler^ UpdateProgWholeHandler;
+	};
+}
