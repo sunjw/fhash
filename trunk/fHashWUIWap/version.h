b3f9a97e93a55a299bba130c108bf63dcf327ff3
Sun Junwen <sunjw8888@gmail.com>
2024-09-06 11:22:22 +0800

    winui: Bump version to 3.9.100.

--- trunk/fHashWUIWap/version.h
+++ trunk/fHashWUIWap/version.h
@@ -1,7 +1,7 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
-#define NUM_VERSION_WUI 3,9,0,0
-#define STR_VERSION_WUI "3.9.0.0"
+#define NUM_VERSION_WUI 3,9,100,0
+#define STR_VERSION_WUI "3.9.100.0"
 
 #endif
