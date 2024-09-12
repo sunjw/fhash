f1f4e136b2964a1052e3fa049c6aff149dfdc126
Sun Junwen <sunjw8888@gmail.com>
2024-09-12 14:50:22 +0800

    uwp: Bump version to 3.3.4.

--- trunk/fHashUwpWap/version.h
+++ trunk/fHashUwpWap/version.h
@@ -1,7 +1,7 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
-#define NUM_VERSION_UWP 3,3,3,0
-#define STR_VERSION_UWP "3.3.3.0"
+#define NUM_VERSION_UWP 3,3,4,0
+#define STR_VERSION_UWP "3.3.4.0"
 
 #endif
