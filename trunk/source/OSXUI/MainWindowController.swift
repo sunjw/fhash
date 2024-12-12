c74a5bf30b278f5978fd3de6d6b1b1126ec87374
Sun Junwen <sunjw8888@gmail.com>
2024-12-12 10:31:37 +0800

    macos: Enable windowFrameAutosaveName to auto save window position.

--- trunk/source/OSXUI/MainWindowController.swift
+++ trunk/source/OSXUI/MainWindowController.swift
@@ -14,6 +14,8 @@ import Cocoa
 
         // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
         window?.delegate = self
+
+        self.windowFrameAutosaveName = "MainWindowFrame"
     }
 
     func getMainViewController() -> MainViewController? {
