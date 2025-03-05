a5489242762cbade2e154b0a8ba566f5c6b07361
Sun Junwen <sunjw8888@gmail.com>
2025-03-05 21:20:42 +0800

    winui: Bump version to 3.11.7.

--- trunk/fHashWUIWap/version.h
+++ trunk/fHashWUIWap/version.h
@@ -1,7 +1,7 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
-#define NUM_VERSION_WUI 3,11,6,0
-#define STR_VERSION_WUI "3.11.6.0"
+#define NUM_VERSION_WUI 3,11,7,0
+#define STR_VERSION_WUI "3.11.7.0"
 
 #endif
