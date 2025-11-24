24b726744ee309a8de6a376bedaf4358dbfc0cdb
Sun Junwen <sunjw8888@gmail.com>
2025-11-24 22:18:39 +0800

    winui: Bump version to 25.12.34.

--- trunk/fHashWUIWap/version.h
+++ trunk/fHashWUIWap/version.h
@@ -1,7 +1,7 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
-#define NUM_VERSION_WUI 25,12,33,0
-#define STR_VERSION_WUI "25.12.33.0"
+#define NUM_VERSION_WUI 25,12,34,0
+#define STR_VERSION_WUI "25.12.34.0"
 
 #endif
