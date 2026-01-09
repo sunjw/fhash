5760e9b260369cf7347eef55622cc13709ac5c6c
Sun Junwen <sunjw8888@gmail.com>
2026-01-09 15:02:19 +0800

    macos: Using create-dmg to package dmg.

--- trunk/package_macos_dmg.sh
+++ trunk/package_macos_dmg.sh
@@ -11,37 +11,47 @@ app_title="fHash"
 app_name="$app_title.app"
 vol_name=$app_title
 application_folder=$1
-temp_dmg_path="$app_title-VERSION-macOS.dmg"
+temp_dmg_name="$app_title-VERSION-macOS.dmg"
 
 echo "Create DMG from [$application_folder]..."
 
 cd "$application_folder"
 
+# Get version
+plist_version=$(/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" "$app_name/Contents/Info.plist")
+temp_dmg_name=${temp_dmg_name/VERSION/$plist_version}
+
 # Clean
 rm -f .DS_Store
 
-# Make /Applications link
-ln -s /Applications Applications
-# Get version
-plist_version=$(/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" "$app_name/Contents/Info.plist")
-temp_dmg_path=${temp_dmg_path/VERSION/$plist_version}
+# # Make /Applications link
+# ln -s /Applications Applications
 
-cd "$cur_dir"
+# cd "$cur_dir"
 
-# Create DMG
-hdiutil create -format UDZO -fs HFS+ -volname "$vol_name" -srcfolder "$application_folder" "$temp_dmg_path"
+# # Create DMG
+# hdiutil create -format UDZO -fs HFS+ -volname "$vol_name" -srcfolder "$application_folder" "$temp_dmg_name"
 
-# Move to target directory
-echo "Move DMG to [$application_folder]"
-mv "$temp_dmg_path" "$application_folder"
+# # Move to target directory
+# echo "Move DMG to [$application_folder]"
+# mv "$temp_dmg_name" "$application_folder"
 
-cd "$application_folder"
+# cd "$application_folder"
 
-# Clean
-rm -f Applications
+# # Clean
+# rm -f Applications
+
+# Call create-dmg
+# Don't forget "npm install --global create-dmg"
+create-dmg --overwrite --no-code-sign "$app_name"
+
+# Rename dmg
+dmg_file=$(ls *.dmg)
+# echo $dmg_file
+mv "$dmg_file" "$temp_dmg_name"
 
 # Tar
-tar -cJvf "$app_name.tar.xz" "$app_name"
+tar -cJvf "$app_name-$plist_version.tar.xz" "$app_name"
 
 cd "$cur_dir"
 
