7498eaf7780d11565d6ba43078cab24f31d34472
Sun Junwen <sunjw8888@gmail.com>
2026-04-20 16:00:22 +0800

    macos: Tweak package script.

--- trunk/package_macos_dmg.sh
+++ trunk/package_macos_dmg.sh
@@ -8,19 +8,30 @@ if [ -z "$1" ]
 fi
 
 cur_dir=$PWD
-app_title="fHash"
-app_name="$app_title.app"
-vol_name=$app_title
 application_folder=$1
-temp_dmg_name="$app_title-VERSION-macOS.dmg"
 
 echo "Create DMG from [$application_folder]..."
 
 cd "$application_folder"
 
+app_name=$(find . -maxdepth 1 -type d -name "*.app" | sort | head -n 1)
+
+if [ -z "$app_name" ]; then
+  echo "No .app found in [$application_folder]"
+  cd "$cur_dir"
+  exit 1
+fi
+
+app_name=${app_name#./}
+app_title=${app_name%.app}
+vol_name=$app_title
+temp_dmg_name="$app_title-VERSION-macOS.dmg"
+
 # Get version
 plist_version=$(/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" "$app_name/Contents/Info.plist")
 temp_dmg_name=${temp_dmg_name/VERSION/$plist_version}
+
+echo "Found app [$app_name], version [$plist_version]"
 
 # Clean
 rm -f .DS_Store
