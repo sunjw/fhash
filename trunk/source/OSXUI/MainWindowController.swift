2442f22afc17a978616f837dda170bcab860e0b5
Sun Junwen <sunjw8888@gmail.com>
2026-04-24 12:56:57 +0800

    macos: Use code to control window size.

--- trunk/source/OSXUI/MainWindowController.swift
+++ trunk/source/OSXUI/MainWindowController.swift
@@ -9,11 +9,14 @@
 import Cocoa
 
 @objc(MainWindowController) class MainWindowController: NoTitlebarWindowController, NSWindowDelegate, NSToolbarDelegate {
+    private static let WindowContentSize = NSSize(width: 568, height: 360)
+
     override func windowDidLoad() {
         super.windowDidLoad()
 
         // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
         window?.delegate = self
+        window?.setContentSize(MainWindowController.WindowContentSize)
 
         if LiquidGlassUI.enableLargeRounded() {
             // Large rounded ui
