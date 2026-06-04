dd6abd7aac8cd2538f30ab3bf6cc0b5ec4cf83b7
Sun Junwen <sunjw8888@gmail.com>
2026-06-04 14:43:39 +0800

    winui: Bump version to 26.7.45.

--- trunk/fHashWUIWap/version.h
+++ trunk/fHashWUIWap/version.h
@@ -1,7 +1,7 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
-#define NUM_VERSION_WUI 26,6,44,0
-#define STR_VERSION_WUI "26.6.44.0"
+#define NUM_VERSION_WUI 26,7,45,0
+#define STR_VERSION_WUI "26.7.45.0"
 
 #endif
