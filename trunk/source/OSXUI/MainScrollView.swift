8a7f5399efc1391b8dcbfd8168086fa664d1dd1e
Sun Junwen <sunjw8888@gmail.com>
2026-06-12 14:35:19 +0800

    Revert "macos: Remove TitlebarView."
    This reverts commit bcf58b345783b9e58b8f431538b495bcde6bba44.

--- trunk/source/OSXUI/MainScrollView.swift
+++ trunk/source/OSXUI/MainScrollView.swift
@@ -10,6 +10,7 @@ import Cocoa
 
 @objc(MainScrollView) class MainScrollView: NSScrollView {
     weak var mainViewController: MainViewController?
+    private var titlebarView: TitlebarView?
 
     override func addSubview(_ view: NSView) {
         let viewClassName = String(describing: type(of: view))
@@ -17,12 +18,12 @@ import Cocoa
 
         if (!MacSwiftUtils.IsSystemEarlierThan(26, 0)) {
             // NSScrollPocket may be added multiple times
-            // let targetViewNames = ["Dummy", /*"NSScrollPocket"*/]
-            // if targetViewNames.contains(viewClassName) {
-            //     DispatchQueue.main.async(execute: { [view] in
-            //
-            //     })
-            // }
+            let targetViewNames = ["Dummy", /*"NSScrollPocket"*/]
+            if targetViewNames.contains(viewClassName) {
+                DispatchQueue.main.async(execute: { [view] in
+                    self.setupTitlebarView(view)
+                })
+            }
         }
 
         // Try to intercept FindBar
@@ -50,4 +51,25 @@ import Cocoa
         }
     }
 
+    private func setupTitlebarView(_ targetView: NSView) {
+        if titlebarView == nil {
+            titlebarView = TitlebarView(frame: CGRect(x: 0,
+                                                      y: 0,
+                                                      width: Int(bounds.width),
+                                                      height: MainScrollView.TitlebarViewHeight))
+            titlebarView?.autoresizingMask = [.width]
+        }
+
+        guard let titlebarView else { return }
+
+        titlebarView.removeFromSuperview()
+        self.addSubview(titlebarView, positioned: .below, relativeTo: targetView)
+
+        NSLayoutConstraint.activate([
+            titlebarView.topAnchor.constraint(equalTo: topAnchor, constant: 0),
+            titlebarView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            titlebarView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            titlebarView.heightAnchor.constraint(equalToConstant: CGFloat(MainScrollView.TitlebarViewHeight))
+        ])
+    }
 }
