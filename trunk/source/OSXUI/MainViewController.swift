0d07c73d901816c4e431b0b93046790386a274c2
Sun Junwen <sunjw8888@gmail.com>
2026-09-16 12:52:39 +0800

    macos: Remove useless code.

--- trunk/source/OSXUI/MainViewController.swift
+++ trunk/source/OSXUI/MainViewController.swift
@@ -390,32 +390,32 @@ private struct MainViewControllerState: OptionSet {
 
         mainTextView.textStorage?.setAttributedString(mainText!)
 
-        if (!MacSwiftUtils.IsSystemEarlierThan(14, 0) &&
-            MacSwiftUtils.IsSystemEarlierThan(15, 3)) {
-            // Sonoma and later insets fix.
-            let fixInset = 5.0
-            let mainTextSize = mainText!.size()
-            let mainScrollViewSize = mainScrollView.frame.size
-            var scrollViewContentInsets = mainScrollView.contentInsets
-            var scrollViewScrollerInsets: NSEdgeInsets?
-            if mainTextSize.width > mainScrollViewSize.width {
-                // Add inset.
-                scrollViewContentInsets.left = fixInset
-                scrollViewContentInsets.right = fixInset
-                scrollViewScrollerInsets = mainScrollView.scrollerInsets
-                scrollViewScrollerInsets?.left = -(fixInset)
-                scrollViewScrollerInsets?.right = -(fixInset)
-            } else {
-                // Reset inset.
-                scrollViewContentInsets.left = 0
-                scrollViewContentInsets.right = 0
-                scrollViewScrollerInsets = mainScrollView.scrollerInsets
-                scrollViewScrollerInsets?.left = 0
-                scrollViewScrollerInsets?.right = 0
-            }
-            mainScrollView.contentInsets = scrollViewContentInsets
-            mainScrollView.scrollerInsets = scrollViewScrollerInsets!
-        }
+        // if (!MacSwiftUtils.IsSystemEarlierThan(14, 0) &&
+        //     MacSwiftUtils.IsSystemEarlierThan(15, 3)) {
+        //     // Sonoma and later insets fix.
+        //     let fixInset = 5.0
+        //     let mainTextSize = mainText!.size()
+        //     let mainScrollViewSize = mainScrollView.frame.size
+        //     var scrollViewContentInsets = mainScrollView.contentInsets
+        //     var scrollViewScrollerInsets: NSEdgeInsets?
+        //     if mainTextSize.width > mainScrollViewSize.width {
+        //         // Add inset.
+        //         scrollViewContentInsets.left = fixInset
+        //         scrollViewContentInsets.right = fixInset
+        //         scrollViewScrollerInsets = mainScrollView.scrollerInsets
+        //         scrollViewScrollerInsets?.left = -(fixInset)
+        //         scrollViewScrollerInsets?.right = -(fixInset)
+        //     } else {
+        //         // Reset inset.
+        //         scrollViewContentInsets.left = 0
+        //         scrollViewContentInsets.right = 0
+        //         scrollViewScrollerInsets = mainScrollView.scrollerInsets
+        //         scrollViewScrollerInsets?.left = 0
+        //         scrollViewScrollerInsets?.right = 0
+        //     }
+        //     mainScrollView.contentInsets = scrollViewContentInsets
+        //     mainScrollView.scrollerInsets = scrollViewScrollerInsets!
+        // }
 
         if !keepScrollPosition {
             // Scroll to end.
